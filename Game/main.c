04cee35b7d1080188a6d229dcbe21bf3b9be1806
Luna Parker <luna@shyowlstudios.com>
2023-03-14 18:43:56 -0400

    Updated project folder names and made main scaffolding

---
+++ Game/main.c
@@ -0,0 +1,13 @@
+// RPS Pro
+// A sophisticated and yet simple take on the classic
+// gesture game - now with AI and a leaderboard!
+//
+// CSCN71030 Section 2, Group 8
+// Michelle Novar, Sierra Erb, Saba Berenji, Luna Parker
+//
+// Please see Git tags and branches for version history
+
+int main(void)
+{
+    return 0;
+}
