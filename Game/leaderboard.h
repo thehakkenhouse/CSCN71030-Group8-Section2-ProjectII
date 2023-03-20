31db6d453f64d8fd57fb2a8483c1c54355279b45
michelle <mnovar9073@conestogac.on.ca>
2023-03-20 07:58:24 -0400

    added linked list.

--- Game/leaderboard.h
+++ Game/leaderboard.h
@@ -19,3 +19,49 @@ typedef struct leaderboard {
 } LEADERBOARD;
 
 #endif
+
+
+
+
+//// build these 
+//struct Node {
+//    int data;
+//    struct Node* next;
+//};
+
+// This function prints contents of linked list starting
+// from the given node
+void printList(struct leaderboardNode* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
+
+// Driver's code
+//int main()
+//{
+    struct leaderboardNode* head = NULL;
+    struct leaderboardNode* second = NULL;
+    struct leaderboardNode* third = NULL;
+
+    // allocate 3 nodes in the heap
+    head = (struct leaderboardNode*)malloc(sizeof(struct leaderboardNode));
+    second = (struct leaderboardNode*)malloc(sizeof(struct leaderboardNode));
+    third = (struct leaderboardNode*)malloc(sizeof(struct leaderboardNode));
+
+    head->data = 1; // assign data in first node
+    head->next = second; // Link first node with second
+
+    second->data = 2; // assign data to second node
+    second->next = third;
+
+    third->data = 3; // assign data to third node
+    third->next = NULL;
+
+    // Function call
+    printList(head);
+
+  /*  return 0;
+}*/
