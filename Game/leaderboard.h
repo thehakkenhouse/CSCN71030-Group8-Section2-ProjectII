f6b3d2bab42beaf31b0c90960f6560357eb5e41e
michelle <mnovar9073@conestogac.on.ca>
2023-03-26 16:39:58 -0400

    added function information

--- Game/leaderboard.h
+++ Game/leaderboard.h
@@ -21,17 +21,28 @@ typedef struct leaderboard {
 
 printLeaderboardByHighestScore();
 // this function will print leaderboard in a sorted order from lowest to highest (max of 10?)
+// communicates with file 
 
 
 doesUsernameExistInDatabase();
 // this function will check if username exists in the leaderboard linked list. will use a while loop
+// communicates with file and authentication?
 
 insertUserIntoLeaderboard();
 // this function will help with new user registration and will insert a new user into the leaderboard
+// communicates with file and authentication?
 
 
 updateUserScore();
-//this function will take a USER pointer to change the score
+// this function will take a USER pointer to change the score
+// communicates with game and file?
+
+exitToMainMenu();
+// player uses to exit leaderboard and go to menu
+// communiacates with menu 
+
+
+
 
 
 // luna said this, but it's not in the game or authentication module. 
@@ -45,6 +56,9 @@ updateUserScore();
 //USER* getUserByUsernameInLeaderboard(const LEADERBOARD* leaderboard, const char username[]);
 //->Authentication module sends username and this function returns either a pointer to a user in the leaderboard, or NULL if the username isn't  in the leaderboard
 
+//goToLeaderboard();
+// user will use to go from menu to leaderboard
+// leaderboard and menu communicate
 
 
 
